f4ac5a35642e9bde07a06a7f7b252c8fa3cbcdcc
Dalton <cdaltonrowe@gmail.com>
2025-04-27 14:58:59 +0200

    setup view component

---
+++ test/components/site_header_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SiteHeaderComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(SiteHeaderComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
