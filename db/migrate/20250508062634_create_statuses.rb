0e03f55bd527cc738f7637e3848af059e0b1ed7b
Dalton <cdaltonrowe@gmail.com>
2025-05-17 16:05:55 +0200

    swap to self contained sections

--- db/migrate/20250508062634_create_statuses.rb
+++ db/migrate/20250508062634_create_statuses.rb
@@ -3,8 +3,7 @@ class CreateStatuses < ActiveRecord::Migration[8.0]
     create_table :statuses do |t|
       t.belongs_to :team, null: false, index: true, foreign_key: true
       t.belongs_to :user, null: false, index: true, foreign_key: true
-      t.json :message
-      t.json :links
+      t.json :sections
 
       t.datetime :updated_at
       t.datetime :created_at
