5ec9265d3781d45260a1909ca27c142ef2190803
Dalton <cdaltonrowe@gmail.com>
2025-05-22 00:32:17 +0200

    show hide button

---
+++ app/javascript/controllers/show_hide_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="show-hide"
+export default class extends Controller {
+  toggle() {
+    const els = this.element.querySelectorAll("details");
+    const allOpen = Array.from(els).every((el) => !!el.getAttribute('open'));
+
+    if (allOpen) {
+      els.forEach(el => el.removeAttribute('open'))
+    } else {
+      els.forEach(el => el.setAttribute('open', 'true'))
+    }
+  }
+}
