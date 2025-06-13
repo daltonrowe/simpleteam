d0a93fa216ecc60c87c3c33ce85f32c80a787e03
Dalton <cdaltonrowe@gmail.com>
2025-06-13 22:18:05 +0200

    style home page teams list lightly

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "302 when logged out" do
+    get dashboard_path
+
+    assert_redirected_to new_session_path
+  end
+
+  test "create team message when no teams" do
+    user = users(:rando)
+
+    sign_in(user)
+    get dashboard_path
+
+    assert_response :success
+    assert_dom "a", "Create a team"
+  end
+
+  test "list all teams when owns team" do
+    user = users(:owner)
+
+    sign_in(user)
+    get dashboard_path
+
+    assert_response :success
+    assert_dom "a", "My Awesome Team"
+    assert_dom "a", "Status Updates"
+    assert_dom "a", "Team Settings"
+  end
+
+  test "list all teams when member of team" do
+    user = users(:member)
+
+    sign_in(user)
+    get dashboard_path
+
+    assert_response :success
+    assert_dom "a", "My Awesome Team"
+    assert_dom "a", "Status Updates"
+  end
 end
