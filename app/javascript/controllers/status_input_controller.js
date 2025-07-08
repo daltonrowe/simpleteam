8f237edc4cd52894b1383fc1cd35ec552d6ca7b8
Dalton <cdaltonrowe@gmail.com>
2025-07-08 21:17:22 +0200

    attach status input qol behaviors

---
+++ app/javascript/controllers/status_input_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="status-input"
+export default class extends Controller {
+  connect() {
+    console.log('hey!');
+
+  }
+
+  focus(event) {
+    if (event.target.value.length === 0) event.target.value = '- '
+  }
+
+  enter(event) {
+    event.preventDefault();
+    event.target.value = event.target.value + "\n- "
+  }
+
+  input(event) {
+    if (event.target.value.length == 0) event.target.value = '- '
+  }
+
+  blur(event) {
+    if (event.target.value.trim() == '-') event.target.value = '';
+  }
+
+  submit() {
+    this.element.requestSubmit();
+  }
+}
