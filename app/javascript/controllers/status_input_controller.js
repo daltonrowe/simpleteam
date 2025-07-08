5d33046a3d55c94a41c49bb3031ba98658fd0fde
Dalton <cdaltonrowe@gmail.com>
2025-07-08 22:24:16 +0200

    handle paste into markdown link

--- app/javascript/controllers/status_input_controller.js
+++ app/javascript/controllers/status_input_controller.js
@@ -11,8 +11,30 @@ export default class extends Controller {
     event.target.value = event.target.value + "\n- "
   }
 
-  input(event) {
-    if (event.target.value.length == 0) event.target.value = '- '
+  beforeInput(event) {
+    if (event.inputType === 'insertFromPaste') {
+      let url;
+
+      try {
+        url = new URL(event.data)
+      } catch (_error) { }
+
+      if (url) this.createMarkdownLink(event)
+    }
+  }
+
+  createMarkdownLink(event) {
+    event.preventDefault()
+
+    const beginning = event.target.value.substring(0, event.target.selectionStart)
+    const middle = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd)
+    const end = event.target.value.substring(event.target.selectionEnd)
+
+    const newValue = beginning + `[${middle}](${event.data})` + end
+
+    // doesn't trigger undo history change, but good enough for now
+    // unsure if suitable alt to execCommand exists
+    event.target.value = newValue
   }
 
   blur(event) {
