220c456bd9608b80255d9ba0f95f9de2ab83f159
Dalton Rowe <cdaltonrowe@gmail.com>
2025-07-09 09:24:06 +0200

    add js linting (#30)
    * add js linting and intentionally fail
    
    * update for ci
    
    * remove intentional error
    
    * remove component tests
    
    * prefer tool-versions
    
    * remove last component test

--- app/javascript/controllers/auto_remove_controller.js
+++ app/javascript/controllers/auto_remove_controller.js
@@ -1,18 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="auto-remove"
 export default class extends Controller {
-
   static values = {
-    duration: Number
-  }
+    duration: Number,
+  };
 
   connect() {
     const duration = this.hasDurationValue ? this.durationValue : 7000;
     const animation = 1000;
 
-    this.element.style.setProperty('--duration', `${duration}ms`)
-    this.element.classList.add('auto-fade')
+    this.element.style.setProperty("--duration", `${duration}ms`);
+    this.element.classList.add("auto-fade");
 
     setTimeout(() => {
       this.element.remove();
