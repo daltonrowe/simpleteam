c4844d1beb9de811a17699d95a6509ef4e1da751
Dalton <cdaltonrowe@gmail.com>
2025-08-30 11:38:19 +0200

    remove unneeded test

--- test/controllers/not_found_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "404 raises an error" do
-    assert_raises(ActionController::RoutingError) do
-      get "/im/not/here"
-    end
-  end
-end
