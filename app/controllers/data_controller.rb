ddd6047ddc276ec2793d14d07383a5b797a76a3a
Dalton <cdaltonrowe@gmail.com>
2025-09-01 20:55:58 +0200

    data explorer page

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,24 +2,5 @@ class DataController < ApplicationController
   before_action :find_team
   user_must_have_seat
 
-  def index
-    # list all data html
-
-    # respond to queries json
-    respond_to do |format|
-      format.html { render }
-    end
-  end
-
-  def show
-    # display a single data record json/html
-  end
-
-  def create
-    # add new data json
-  end
-
-  def destory
-    # add new data
-  end
+  def index;end
 end
