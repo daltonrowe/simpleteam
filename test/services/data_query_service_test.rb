a20047b3d3263bf2fec848504dc9ee3af5728bd2
Dalton <cdaltonrowe@gmail.com>
2025-09-01 21:44:15 +0200

    start data query service

---
+++ test/services/data_query_service_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class DataQueryServiceTest < ActiveSupport::TestCase
+  # test "should call service" do
+  #   result = DataQuery.call
+  #   assert_equal @data_query, result
+  # end
+end
