923a5f2f6ece3ea6aee5a9b65cfa8fb131574184
Dalton <cdaltonrowe@gmail.com>
2025-09-03 07:49:43 +0200

    start data store capability

--- app/services/data_query_service.rb
+++ app/services/data_query_service.rb
@@ -1,13 +1,30 @@
 class DataQueryService
+  DEFAULT_QUERY_PARAMS = {
+    per_page: 30,
+    page: 1,
+    name: nil,
+    order: "desc"
+  }.freeze
+
   def initialize(team:, params:)
     @team = team
-    @params = params
+    @params = params.to_h.symbolize_keys
   end
 
   attr_reader :data_query
 
   def call
-    @data_query = Datum.where(team: @team)
+    query_params = DEFAULT_QUERY_PARAMS.merge(@params)
+
+    where_args = {
+      team: @team
+    }
+
+    where_args[:name] = query_params[:name] if query_params[:name]
+
+    Datum.where(**where_args)
+      .order(created_at: query_params[:order].to_sym)
+      .limit(query_params[:per_page])
   end
 
   private
--- test/controllers/data_controller_test.rb
+++ test/controllers/data_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class DataControllerTest < ActionDispatch::IntegrationTest
+  correct_headers = {
+      "Accept": "application/json",
+      "X-API-Key": "the-correct-key"
+    }
   test "returns a 406 unacceptable unless request is json" do
     team = teams(:basic)
 
@@ -22,10 +26,47 @@ class DataControllerTest < ActionDispatch::IntegrationTest
     assert_response :bad_request
   end
 
+  # index
+
   test "returns a 200 with correct key" do
     team = teams(:with_api_key)
 
-    get team_data_path(team), headers: { "Accept": "application/json", "X-API-Key": "the-correct-key" }
+    get team_data_path(team), headers: correct_headers
     assert_response :ok
+    data = JSON.parse response.body
+
+    assert_equal 3, data.length
+    assert DateTime.parse(data[0]["created_at"]).after? DateTime.parse(data[1]["created_at"])
+  end
+
+  test "returns the correct amount per page" do
+    team = teams(:with_api_key)
+
+    get(
+      team_data_path(team),
+        headers: correct_headers,
+        params: { per_page: 2 }
+    )
+
+    assert_response :ok
+    data = JSON.parse response.body
+
+    assert_equal 2, data.length
+  end
+
+  test "returns data with the correct name" do
+    team = teams(:with_api_key)
+
+    get(
+      team_data_path(team),
+        headers: correct_headers,
+        params: { name: "Another Data Type" }
+    )
+
+    assert_response :ok
+    data = JSON.parse response.body
+
+    assert_equal 1, data.length
+    assert_equal "Another Data Type", data[0]["name"]
   end
 end
