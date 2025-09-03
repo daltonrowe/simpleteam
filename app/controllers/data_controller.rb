d85f66d2592205adf3fc0fb4cf4ee2cfd43f534e
Dalton <cdaltonrowe@gmail.com>
2025-09-03 07:49:43 +0200

    start data store capability

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -5,11 +5,20 @@ class DataController < ApplicationController
   before_action :validate_api_key
 
   def index
-    data = Datum.where(team: @team).all
+    data = DataQueryService.new(team: @team, params: query_params).call
     render json: data.to_json
   end
 
+  def names
+    names = Datum.where(team: @team).select(:name).distinct
+    render json: names.to_json
+  end
+
   private
+
+  def query_params
+    params.permit(:name, :per_page, :page)
+  end
 
   def validate_api_key
     head :bad_request unless @team.data_api_key && request.headers["X-API-Key"] == @team.data_api_key
