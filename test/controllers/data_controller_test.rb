c11d98513437ce95ae27b3407d1bc957da5a491e
Dalton <cdaltonrowe@gmail.com>
2025-09-06 07:46:46 +0100

    ensure content type

--- test/controllers/data_controller_test.rb
+++ test/controllers/data_controller_test.rb
@@ -6,24 +6,24 @@ class DataControllerTest < ActionDispatch::IntegrationTest
       "Content-Type": "application/json",
       "X-API-Key": "the-correct-key"
     }
-  test "returns a 406 unacceptable unless request is json" do
+  test "returns a 406 not acceptable unless request is json" do
     team = teams(:basic)
 
     get team_data_path(team)
     assert_response :not_acceptable
   end
 
-  test "returns a 400 bad request" do
+  test "returns a 400 bad request without key" do
     team = teams(:basic)
 
-    get team_data_path(team), headers: { "Accept": "application/json" }
+    get team_data_path(team), headers: { "Accept": "application/json", "Content-Type": "application/json" }
     assert_response :bad_request
   end
 
   test "returns a 400 bad request with incorrect key" do
     team = teams(:with_api_key)
 
-    get team_data_path(team), headers: { "Accept": "application/json", "X-API-Key": "this-is-wrong" }
+    get team_data_path(team), headers: { "Content-Type": "application/json", "Accept": "application/json", "X-API-Key": "this-is-wrong" }
     assert_response :bad_request
   end
 
