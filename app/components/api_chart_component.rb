37bc6c56d8b2a64604ccdcb93dfa807f05bcac53
Dalton <cdaltonrowe@gmail.com>
2025-09-22 14:37:23 +0200

    use call

--- app/components/api_chart_component.rb
+++ app/components/api_chart_component.rb
@@ -10,4 +10,13 @@ class ApiChartComponent < ApplicationComponent
   end
 
   attr_accessor :name, :team, :keys
+
+  def call
+    tag.div class: "aspect-video bg-yin-800 rounded-lg", data: {
+      api_chart_name_value: name,
+      api_chart_team_value: team.id,
+      api_chart_api_key_value: team.data_api_key,
+      api_chart_keys_value: keys
+    }
+  end
 end
