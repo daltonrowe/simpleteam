217a102b4b5a647ee1766e5196b2907f273c6bd4
Marco Adkins <10146618+marcoadkins@users.noreply.github.com>
2025-10-03 09:16:31 -0400

    Add daily slack notification jobs and tests

---
+++ test/jobs/daily_schedule_list_status_job_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class DailyScheduleListStatusJobTest < ActiveJob::TestCase
+  setup do
+    @slack_installation = slack_installations(:default)
+    @slack_team = teams(:slack_team)
+  end
+
+  test "schedules list status job for each slack installation" do
+    DailyScheduleListStatusJob.perform_now
+    assert_enqueued_with(job: SlackListStatusJob, args: [@slack_team.id], at: Time.parse('09:00 UTC'))
+  end
+end
