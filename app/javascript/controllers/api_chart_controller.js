c8458a3c00a3c13dbda758952048d79e15fdfa85
Dalton Rowe <cdaltonrowe@gmail.com>
2025-10-24 16:17:50 +0200

    keep scale consistent (#55)

--- app/javascript/controllers/api_chart_controller.js
+++ app/javascript/controllers/api_chart_controller.js
@@ -61,6 +61,7 @@ export default class extends Controller {
             },
           },
           y: {
+            beginAtZero: true,
             ticks: {
               color: "#DDD",
             },
