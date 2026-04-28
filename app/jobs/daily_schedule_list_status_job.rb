fed097db67bc8270d79150c94589b59d482f98eb
Dalton <cdaltonrowe@gmail.com>
2026-04-28 09:50:26 +0200

    perform list in time zone?

--- app/jobs/daily_schedule_list_status_job.rb
+++ app/jobs/daily_schedule_list_status_job.rb
@@ -2,7 +2,9 @@ class DailyScheduleListStatusJob < ApplicationJob
   def perform
     SlackInstallation.all.each do |installation|
       installation.teams.each do |team|
-        SlackListStatusJob.set(wait_until: team.notification_time).perform_later(team.id)
+        team.in_team_zone do
+          SlackListStatusJob.set(wait_until: team.notification_time).perform_later(team.id)
+        end
       end
     end
   end
