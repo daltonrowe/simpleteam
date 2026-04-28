9319eb9ecf0456d9794de23894ab926ab2772e47
Dalton <cdaltonrowe@gmail.com>
2026-04-28 09:56:57 +0200

    kick over into next day instead of firing immediately

--- app/jobs/daily_schedule_list_status_job.rb
+++ app/jobs/daily_schedule_list_status_job.rb
@@ -2,9 +2,7 @@ class DailyScheduleListStatusJob < ApplicationJob
   def perform
     SlackInstallation.all.each do |installation|
       installation.teams.each do |team|
-        team.in_team_zone do
-          SlackListStatusJob.set(wait_until: team.notification_time).perform_later(team.id)
-        end
+        SlackListStatusJob.set(wait_until: team.notification_time).perform_later(team.id)
       end
     end
   end
